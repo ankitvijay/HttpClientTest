51bcdcfe8434a50153c7161ed937010bf059185c
anvijay <anvijay@VIJAY-ANKIT>
2016-09-25 21:19:29 +0530

    Initial Checkin

---
+++ HttpClientTest/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace HttpClientTest
+{
+    using System;
+    using System.Net.Http;
+
+    class Program
+    {
+        private static readonly int _connections = 10;
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        private static void Main()
+        {
+            //TestHttpClientWithStaticInstance();
+            TestHttpClientWithUsing();
+        }
+
+        private static void TestHttpClientWithUsing()
+        {
+            try
+            {
+                for (var i = 0; i < _connections; i++)
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        var result = httpClient.GetAsync(new Uri("http://bing.com")).Result;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private static void TestHttpClientWithStaticInstance()
+        {
+            try
+            {
+                for (var i = 0; i < _connections; i++)
+                {
+                    var result = _httpClient.GetAsync(new Uri("http://bing.com")).Result;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
